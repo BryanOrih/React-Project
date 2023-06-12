55efdfa2beb7c281c616681c7cdde0fa217f9269
Bryan Alvarez <balvarez@LABA.PSWAS.LOCAL>
2023-06-12 16:27:53 -0700

    eg

--- widgetai/src/components/WidgetCreation/InputComponents/OptionalInputs/index.js
+++ widgetai/src/components/WidgetCreation/InputComponents/OptionalInputs/index.js
@@ -1,58 +1,67 @@
-import React, { useState } from 'react'
-import "./index.css"
+import React, { useState } from 'react';
+import "./index.css";
+
 const OptionalInputs = () => {
-  const [input, setInput] = useState({})
-  const [populateArray, setPopulateArray] = useState([1])
-  const handleInput = (e) =>{
-    let name = e.target.name
-    setInput({...input, [`${name}`]:e.target.value})
-  }
-  const handleAdd = (target) =>{
-    setPopulateArray([...populateArray, target])
-  }
-  const handleMinus = (target) =>{
-    let subArray = [...populateArray]
-    // let num = target
-    let index = subArray.indexOf(target)
-    subArray.splice(index,1)
-    setPopulateArray([...subArray])
-  }
-  const populate = populateArray.map((value, index)=>{
-    let random = Math.random()
-    let name = `OptionalInputs${random}`
-    return(
+  const [input, setInput] = useState({});
+  const [populateArray, setPopulateArray] = useState([0]);
+
+  const handleInput = (e) => {
+    setInput({ ...input, [`${e.target.name}`]: e.target.value });
+  };
+
+  const handleAdd = (target) => {
+    setPopulateArray([...populateArray, target]);
+  };
+
+  const handleMinus = (index) => {
+    let subArray = [...populateArray];
+    subArray.splice(index, 1);
+    setPopulateArray([...subArray]);
+  };
+
+  const populate = populateArray.map((value, index) => {
+    let random = Math.ceil(Math.random() * 100000000);
+    let name = `OptionalInputs${random}`;
+    return (
       <div className='OptionInputContainer' key={value + index}>
         <div className='OptionInputDivDesign'>
-            <div className='PromptContainer'>
-              <label htmlFor='OptionalInputs'>
-                Entry User Information (Optional)
-              </label>
-              <input name={`${name}`} type='text' placeholder='Ex: Full Name' onChange={handleInput} value={input[name]}/>  
-            </div>
-            <div className='CatContainer'>
-              <label htmlFor='OptionalInputsCat'>
-                Category
-              </label>
-              <select name="OptionalInputsCat" type='text'>  
-                <option value="None">Select</option>
-                <option value="Country">Country</option>
-                <option value="Name">Name</option>
-                <option value="Request/Message">Request/Message</option>
-                <option value="PhoneNumber">Phone Number</option>
-                <option value="Email">Email</option>
-              </select>
-            </div>
-            <i className="fa-solid fa-square-plus addBtn" onClick={(name)=>{handleAdd(name)}}></i>
-            <i className="fa-solid fa-square-minus minusBtn" onClick={(name)=>{handleMinus(name)}}></i>
-        </div>                                         
+          <div className='PromptContainer'>
+            <label htmlFor='OptionalInputs'>
+              Entry User Information (Optional)
+            </label>
+            <input
+              name={name}
+              type='text'
+              placeholder='Ex: Full Name'
+              onChange={handleInput}
+              value={input[name]}
+            />
+          </div>
+          <div className='CatContainer'>
+            <label htmlFor='OptionalInputsCat'>
+              Category
+            </label>
+            <select name="OptionalInputsCat" type='text'>
+              <option value="None">Select</option>
+              <option value="Country">Country</option>
+              <option value="Name">Name</option>
+              <option value="Request/Message">Request/Message</option>
+              <option value="PhoneNumber">Phone Number</option>
+              <option value="Email">Email</option>
+            </select>
+          </div>
+          <i className="fa-solid fa-square-plus addBtn" onClick={() => { setPopulateArray([...populateArray, name]) }}></i>
+          <i className="fa-solid fa-square-minus minusBtn" onClick={() => { handleMinus(index) }}></i>
+        </div>
       </div>
-    )
-  })
+    );
+  });
+
   return (
     <>
       {populate}
     </>
-  )
+  );
 }
 
-export default OptionalInputs
+export default OptionalInputs;
